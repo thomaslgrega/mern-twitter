6b874a4767669d26efe2dd1af068a6be2e6e6ed8
tgrega <zeroyontlg@yahoo.com>
2020-08-29 15:50:30 -0700

    Finish backend tweets and users

--- app.js
+++ app.js
@@ -5,13 +5,22 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello again World"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
