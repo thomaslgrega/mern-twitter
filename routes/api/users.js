fbffa09a18e88aa267cd252fedd13e4bc11e2108
tgrega <zeroyontlg@yahoo.com>
2020-08-30 09:59:22 -0700

    Fixed search by handle to search for email when logging in

--- routes/api/users.js
+++ routes/api/users.js
@@ -66,10 +66,10 @@ router.post("/login", (req, res) => {
     return res.status(400).json(errors);
   }
 
-  const handle = req.body.handle;
+  const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({ handle }).then(user => {
+  User.findOne({ email }).then(user => {
     if (!user) {
       errors.handle = "This user does not exist";
       return res.status(400).json(errors);
